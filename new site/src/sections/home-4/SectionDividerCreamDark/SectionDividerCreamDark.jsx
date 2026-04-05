624d108fc87568ccaeaccaa2aa83376f6e4fb507
priyamfree-netizen <priyamfree@gmail.com>
2026-04-05 13:19:03 +0530

    vv

--- new site/src/sections/home-4/SectionDividerCreamDark/SectionDividerCreamDark.jsx
+++
@@ -1,9 +0,0 @@
-const SectionDividerCreamDark = () => {
-  return (
-    <div className="section-divider bg-cream">
-      <img src="/image/home-4/section-divider-gray.svg" alt="section divider" />
-    </div>
-  );
-};
-
-export default SectionDividerCreamDark;
